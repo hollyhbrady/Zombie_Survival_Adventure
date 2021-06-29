9b7f7fd6ab28f24664189a2a8a4f5337101a5f61
hollyhbrady <hollyhbrady@gmail.com>
2021-06-29 17:52:55 +0100

    Loot class done

--- src/main/java/models/loot/Loot.java
+++ src/main/java/models/loot/Loot.java
@@ -1,4 +1,38 @@
 package models.loot;
 
 public class Loot {
+
+    private Weapon weapon;
+    private Food food;
+    private int ammo;
+
+    public Loot(Weapon weapon, Food food, int ammo) {
+        this.weapon = weapon;
+        this.food = food;
+        this.ammo = ammo;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
+
+    public int getAmmo() {
+        return ammo;
+    }
+
+    public void setAmmo(int ammo) {
+        this.ammo = ammo;
+    }
 }
