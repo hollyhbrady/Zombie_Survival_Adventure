e5016069dbaec9b2a11df142d2f586c68ba1d10d
hollyhbrady <hollyhbrady@gmail.com>
2021-06-29 18:22:58 +0100

    Game update - can pick up food from zombie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -105,6 +105,46 @@ public class Main {
                     System.out.println("Invalid command.");
                 }
             }
+            if (health < 1) {
+                System.out.println("You limp back to your hut, will you be next to join the ranks of the infected?");
+                break;
+            }
+            System.out.println("------------------------------");
+            System.out.println("The Shambler was defeated!");
+            zombiesDefeated++;
+            if (rand.nextInt(100) < foodDropChance) {
+                numFood++;
+                System.out.println("It dropped a can of baked beans!");
+                System.out.println("You now have " + numFood + " cans of beans!.");
+            }
+            if (rand.nextInt(100) < snackDropChance) {
+                numSnack++;
+                System.out.println("It dropped a pack of dried fruit!");
+                System.out.println("You now have " + numSnack + " packs!.");
+            }
+            if (rand.nextInt(100) < drinkDropChance) {
+                numDrink++;
+                System.out.println("It dropped a can of Irn-Bru!!");
+                System.out.println("You now have " + numDrink + " cans. If only they had nutritional value...");
+            }
+            System.out.println("------------------------------");
+            System.out.println("What would you like to do?");
+            System.out.println("1. Continue on your path");
+            System.out.println("2. Return to hut");
+
+            String input = in.nextLine();
+
+            while(!input.equals("1") && !input.equals("2")) {
+                System.out.println("Invalid command");
+                input = in.nextLine();
+            }
+            if (input.equals("1")) {
+                System.out.println("You continue further down the path.");
+            }
+            else if (input.equals("2")) {
+                System.out.println("You return to your hut for a rest.");
+                break;
+            }
         }
         System.out.println("You defeated " + zombiesDefeated + " Zombies, fled from " + zombieFledCount + " and used " + healthRestoresUsed + " HP restores.");
         System.out.println("Thanks for playing!");
