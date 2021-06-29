4427d4d4bc5c40a5ce6bdb980f82dbe56e8d66b2
hollyhbrady <hollyhbrady@gmail.com>
2021-06-29 16:06:39 +0100

    Survivor and Zombie classes set-up

--- src/main/java/models/Survivor.java
+++ src/main/java/models/Survivor.java
@@ -2,6 +2,7 @@ package models;
 
 import models.loot.Loot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Survivor {
@@ -9,5 +10,46 @@ public class Survivor {
     private Long id;
     private String name;
     private List<Loot> inventory;
+    private SurvivorLevels sLevel;
 
+    public Survivor(String name, List<Loot> inventory, SurvivorLevels sLevel) {
+        this.name = name;
+        this.inventory = new ArrayList<>();
+        this.sLevel = sLevel;
+    }
+
+    public Survivor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Loot> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<Loot> inventory) {
+        this.inventory = inventory;
+    }
+
+    public SurvivorLevels getsLevel() {
+        return sLevel;
+    }
+
+    public void setsLevel(SurvivorLevels sLevel) {
+        this.sLevel = sLevel;
+    }
 }
--- src/main/java/models/Zombie.java
+++ src/main/java/models/Zombie.java
@@ -1,4 +1,30 @@
 package models;
 
 public class Zombie {
+
+    private Long id;
+    private ZombieLevels zLevel;
+
+    public Zombie(ZombieLevels zLevel) {
+        this.zLevel = zLevel;
+    }
+
+    public Zombie() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ZombieLevels getzLevel() {
+        return zLevel;
+    }
+
+    public void setzLevel(ZombieLevels zLevel) {
+        this.zLevel = zLevel;
+    }
 }
