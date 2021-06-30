b65c1b645e2b207e3cb2fe5012adb421f69edb47
hollyhbrady <hollyhbrady@gmail.com>
2021-06-30 15:26:49 +0100

    updated survivorLevel to work with checking accuracy

--- src/main/java/models/SurvivorLevel.java
+++ src/main/java/models/SurvivorLevel.java
@@ -3,17 +3,17 @@ package models;
 public enum SurvivorLevel {
 
     // BEST GUN ACCURACY
-    SOLDIERONE(50, 0.8, 0.7,20, 10),
-    SOLDIERTWO(75, 0.9, 0.8,25, 15),
-    SOLDIERTHREE(100, 0.95, 0.9,30, 20),
+    SOLDIERONE(50, 80, 0.7,20, 10),
+    SOLDIERTWO(75, 90, 0.8,25, 15),
+    SOLDIERTHREE(100, 95, 0.9,30, 20),
     // FASTEST
-    WANDERERONE(50, 0.7, 0.7, 20, 15),
-    WANDERERTWO(75, 0.8, 0.8,25, 25),
-    WANDERERTHREE(100, 0.9, 0.9,30, 30),
+    WANDERERONE(50, 70, 0.7, 20, 15),
+    WANDERERTWO(75, 80, 0.8,25, 25),
+    WANDERERTHREE(100, 90, 0.9,30, 30),
     // BEST MELEE ACCURACY
-    OUTCASTONE(50, 0.7, 0.8,20, 10),
-    OUTCASTTWO(75, 0.8, 0.9,25, 15),
-    OUTCASTTHREE(100, 0.9, 0.95,30, 20);
+    OUTCASTONE(50, 70, 0.8,20, 10),
+    OUTCASTTWO(75, 80, 0.9,25, 15),
+    OUTCASTTHREE(100, 90, 0.95,30, 20);
 
     private int HP;
     private double gunAccuracy;
