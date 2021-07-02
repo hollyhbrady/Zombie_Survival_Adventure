601440ee43501f6b88609a054e5e125da903dca3
hollyhbrady <hollyhbrady@gmail.com>
2021-07-02 15:44:38 +0100

    Create Survivor and create zombie tests

--- Zombie_Survival_Adventure/src/test/java/Holly/Zombie_Survival_Adventure/ZombieSurvivalAdventureApplicationTests.java
+++ Zombie_Survival_Adventure/src/test/java/Holly/Zombie_Survival_Adventure/ZombieSurvivalAdventureApplicationTests.java
@@ -1,5 +1,10 @@
 package Holly.Zombie_Survival_Adventure;
 
+import Holly.Zombie_Survival_Adventure.models.Survivor;
+import Holly.Zombie_Survival_Adventure.models.Zombie;
+import com.sun.tools.internal.jxc.SchemaGenerator;
+import models.SurvivorLevel;
+import models.ZombieLevel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +13,16 @@ class ZombieSurvivalAdventureApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createSurvivor() {
+		Survivor holly = new Survivor("Holly", 6, SurvivorLevel.OUTCASTONE, 50);
+	}
+
+	@Test
+	public void createZombie() {
+		Zombie runner = new Zombie("Runner", ZombieLevel.THREE);
 	}
 
 }
