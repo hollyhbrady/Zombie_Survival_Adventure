5dd07900367c28ca31488fd6c0c04458f8f9e1e6
hollyhbrady <hollyhbrady@gmail.com>
2021-07-02 11:59:33 +0100

    Spring

---
+++ Zombie_Survival_Adventure/src/main/java/Holly/Zombie_Survival_Adventure/ZombieSurvivalAdventureApplication.java
@@ -0,0 +1,13 @@
+package Holly.Zombie_Survival_Adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombieSurvivalAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZombieSurvivalAdventureApplication.class, args);
+	}
+
+}
---
+++ Zombie_Survival_Adventure/src/test/java/Holly/Zombie_Survival_Adventure/ZombieSurvivalAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package Holly.Zombie_Survival_Adventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZombieSurvivalAdventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
