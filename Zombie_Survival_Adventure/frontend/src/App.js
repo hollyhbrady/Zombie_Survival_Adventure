aaa27728020df13872495c35c7009d769a6fc848
hollyhbrady <hollyhbrady@gmail.com>
2021-07-04 20:47:43 +0100

    end point sunday

--- Zombie_Survival_Adventure/frontend/src/App.js
+++ Zombie_Survival_Adventure/frontend/src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Player from "./components/Player";
+import Game from "./components/Game";
+
 function App() {
   return (
     <div className="Game-start">
