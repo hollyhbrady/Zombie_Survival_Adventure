dc0701d159a011c5f0623eb72863828ec19fb329
hollyhbrady <hollyhbrady@gmail.com>
2021-07-05 17:56:58 +0100

    Survivorlevel controller and getAll

---
+++ Zombie_Survival_Adventure/src/main/java/Holly/Zombie_Survival_Adventure/controllers/SurvivorLevelController.java
@@ -0,0 +1,22 @@
+package Holly.Zombie_Survival_Adventure.controllers;
+
+import Holly.Zombie_Survival_Adventure.models.Survivor;
+import Holly.Zombie_Survival_Adventure.models.SurvivorLevel;
+import Holly.Zombie_Survival_Adventure.repositories.SurvivorLevelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SurvivorLevelController {
+
+    @Autowired
+    SurvivorLevelRepository survivorLevelRepository;
+
+    @GetMapping(value = "/survivorLevel")
+    public List<SurvivorLevel> getAllSurvivorLevels() {
+        return survivorLevelRepository.findAll();
+    }
+}
