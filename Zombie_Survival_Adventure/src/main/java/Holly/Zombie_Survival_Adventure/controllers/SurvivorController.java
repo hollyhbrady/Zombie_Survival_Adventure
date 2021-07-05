e14ff5cde4e602f6633cc6f420cff84de4c58ab3
hollyhbrady <hollyhbrady@gmail.com>
2021-07-05 17:17:00 +0100

    Post new survivor

--- Zombie_Survival_Adventure/src/main/java/Holly/Zombie_Survival_Adventure/controllers/SurvivorController.java
+++ Zombie_Survival_Adventure/src/main/java/Holly/Zombie_Survival_Adventure/controllers/SurvivorController.java
@@ -3,7 +3,11 @@ package Holly.Zombie_Survival_Adventure.controllers;
 import Holly.Zombie_Survival_Adventure.models.Survivor;
 import Holly.Zombie_Survival_Adventure.repositories.SurvivorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +21,11 @@ public class SurvivorController {
     @GetMapping(value = "/survivors")
     public List<Survivor> getAllSurvivors() {
         return survivorRepository.findAll();
+    }
+
+    @PostMapping(value = "/survivors")
+    public ResponseEntity<Survivor> postSurvivor(@RequestBody Survivor survivor){
+        survivorRepository.save(survivor);
+        return new ResponseEntity<>(survivor, HttpStatus.CREATED);
     }
 }
