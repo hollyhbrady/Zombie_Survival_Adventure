7cb18132d7e3468756834c3d9bbc4258c84a6213
hollyhbrady <hollyhbrady@gmail.com>
2021-07-06 19:38:50 +0100

    edit loot package

--- Zombie_Survival_Adventure/src/main/java/Holly/Zombie_Survival_Adventure/models/loot/Loot.java
+++ Zombie_Survival_Adventure/src/main/java/Holly/Zombie_Survival_Adventure/models/loot/Loot.java
@@ -1,40 +1,40 @@
-package models.loot;
+package Holly.Zombie_Survival_Adventure.models.loot;
 
 public abstract class Loot {
 
-    private Gun gun;
-    private Melee melee;
-    public Food food;
+    private models.loot.Gun gun;
+    private models.loot.Melee melee;
+    public models.loot.Food food;
     private int ammo;
 
-    public Loot(Gun gun, Melee melee, Food food, int ammo) {
+    public Loot(models.loot.Gun gun, models.loot.Melee melee, models.loot.Food food, int ammo) {
         this.gun = gun;
         this.melee = melee;
         this.food = food;
         this.ammo = ammo;
     }
 
-    public Gun getGun() {
+    public models.loot.Gun getGun() {
         return gun;
     }
 
-    public void setGun(Gun gun) {
+    public void setGun(models.loot.Gun gun) {
         this.gun = gun;
     }
 
-    public Melee getMelee() {
+    public models.loot.Melee getMelee() {
         return melee;
     }
 
-    public void setMelee(Melee melee) {
+    public void setMelee(models.loot.Melee melee) {
         this.melee = melee;
     }
 
-    public Food getFood() {
+    public models.loot.Food getFood() {
         return food;
     }
 
-    public void setFood(Food food) {
+    public void setFood(models.loot.Food food) {
         this.food = food;
     }
 
