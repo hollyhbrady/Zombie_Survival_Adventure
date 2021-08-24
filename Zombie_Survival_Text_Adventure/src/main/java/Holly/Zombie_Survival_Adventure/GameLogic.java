32d2311394e6f0ca718678d98479fed47e18008a
hollyhbrady <hollyhbrady@gmail.com>
2021-08-24 18:10:51 +0100

    drop6Bullets()

--- Zombie_Survival_Text_Adventure/src/main/java/Holly/Zombie_Survival_Adventure/GameLogic.java
+++ Zombie_Survival_Text_Adventure/src/main/java/Holly/Zombie_Survival_Adventure/GameLogic.java
@@ -240,6 +240,16 @@ public class GameLogic {
         System.out.println("'That's one sweet looking knife there, don't mind if I do!'");
     }
 
+    public static void drop6Bullets(Survivor survivor, Zombie zombie) {
+        survivor.addToAmmoInventory(6);
+        System.out.println("A full magazine! That means 6 bullets.");
+        if (survivor.getAmmoInventory() == 0) {
+            System.out.println("'Phew I was all out, this will last me a while'");
+        } else {
+            System.out.println("'These Zed Heads better watch out!'");
+        }
+    }
+
     public static void drop4Bullets(Survivor survivor, Zombie zombie) {
         survivor.addToAmmoInventory(4);
         System.out.println("A magazine with 4 bullets fell from the " + zombie.getName() + "'s belt during the fight!");
